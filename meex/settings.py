8d634051217bf6806015c1e5d6bf74b8e59a8845
silvano valdez <silvanovaldez90@gmail.com>
2020-06-21 10:00:23 -0500

    second

--- meex/settings.py
+++ meex/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIR = [STATIC_DIR]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
